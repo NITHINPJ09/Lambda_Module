cbdc3de4496f52b19c55890412b041a6b99eb840
Nithin Prince John <nithinpj09@gmail.com>
2023-12-07 17:13:39 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -7,20 +7,6 @@ data "aws_s3_object" "existing_lambda_object" {
   key    = var.lambda_code_zip
 }
 
-data "aws_s3_object" "existing_lambda_layer" {
-  bucket = data.aws_s3_bucket.existing_lambda_bucket.bucket
-  key    = "python.zip" # Replace with the actual key of your existing S3 object
-}
-
-resource "aws_lambda_layer_version" "existing_lambda_layer" {
-  s3_bucket           = data.aws_s3_bucket.existing_lambda_bucket.id
-  s3_key              = data.aws_s3_object.existing_lambda_layer.key
-  layer_name          = "lambda_package"
-  compatible_runtimes = ["python3.8"]
-  skip_destroy        = true
-  s3_object_version = data.aws_s3_object.existing_lambda_layer.version_id
-}
-
 resource "aws_lambda_function" "terraform_lambda_func" {
   function_name    = var.lambda_function_name
   role             = var.lambda_role_arn
