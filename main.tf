28497020ed29ea660823def7e16126dc0a48fbed
Nithin Prince John <nithinpj09@gmail.com>
2023-12-07 17:15:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ module "FirstLambda" {
   lambda_function_name = var.lambda_first_function_name
   handler              = "hello-python.lambda_handler"
   environment          = var.environment
+  layer_arn            = aws_lambda_layer_version.existing_lambda_layer.arn
 }
 
 module "SecondLambda" {
@@ -34,4 +35,5 @@ module "SecondLambda" {
   lambda_function_name = var.lambda_second_function_name
   handler              = "hi-python.lambda_handler"
   environment          = var.environment
+  layer_arn            = aws_lambda_layer_version.existing_lambda_layer.arn
 }
