74b2b442bd47ed66ca1ee124441be07e59406f0b
Nithin Prince John <nithinpj09@gmail.com>
2023-12-08 13:13:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-data "aws_s3_bucket" "existing_lambda_bucket" {
-  bucket = var.lambda_bucket # Replace with the actual name of your existing S3 bucket
-}
-
-data "aws_s3_object" "existing_lambda_layer" {
-  bucket = data.aws_s3_bucket.existing_lambda_bucket.bucket
-  key    = "python.zip" # Replace with the actual key of your existing S3 object
-}
-
-resource "aws_lambda_layer_version" "existing_lambda_layer" {
-  s3_bucket           = data.aws_s3_bucket.existing_lambda_bucket.id
-  s3_key              = data.aws_s3_object.existing_lambda_layer.key
-  layer_name          = "my_layer"
-  compatible_runtimes = ["python3.8"]
-  skip_destroy        = false
-  s3_object_version   = data.aws_s3_object.existing_lambda_layer.version_id
-}
-
 module "FirstLambda" {
   source               = "./modules/lambda"
   lambda_role_arn      = aws_iam_role.lambda_role.arn
