254c88aa41651979a29b1f80a075f024be9e8f41
Nithin Prince John <nithinpj09@gmail.com>
2023-12-08 13:29:08 +0530

    Update layer.tf

--- layer.tf
+++ layer.tf
@@ -10,7 +10,7 @@ data "aws_s3_object" "existing_lambda_layer" {
 resource "aws_lambda_layer_version" "existing_lambda_layer" {
   s3_bucket           = data.aws_s3_bucket.existing_lambda_bucket.id
   s3_key              = data.aws_s3_object.existing_lambda_layer.key
-  layer_name          = "my_layer"
+  layer_name          = "custom_layer"
   compatible_runtimes = ["python3.8"]
   skip_destroy        = false
   s3_object_version   = data.aws_s3_object.existing_lambda_layer.version_id
