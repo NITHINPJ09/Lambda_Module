8901772e81a1a274bcdbcb6d6adaae76a517a0d1
Nithin Prince John <nithinpj09@gmail.com>
2023-12-20 11:28:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 module "FirstLambda" {
   source               = "./modules/lambda"
-  lambda_role_arn      = aws_iam_role.lambda_role.arn
   lambda_bucket        = var.lambda_bucket
   lambda_code_zip      = var.lambda_code_first_zip
   lambda_function_name = var.lambda_first_function_name
@@ -11,7 +10,6 @@ module "FirstLambda" {
 
 module "SecondLambda" {
   source               = "./modules/lambda"
-  lambda_role_arn      = aws_iam_role.lambda_role.arn
   lambda_bucket        = var.lambda_bucket
   lambda_code_zip      = var.lambda_code_second_zip
   lambda_function_name = var.lambda_second_function_name
