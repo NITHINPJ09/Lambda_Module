bcf1a2c2183f00b69dc746ca7771169a1600b89a
Nithin Prince John <nithinpj09@gmail.com>
2023-12-20 11:41:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+module "network" {
+  source = "./modules/network"
+}
+
 module "FirstLambda" {
   source               = "./modules/lambda"
   lambda_bucket        = var.lambda_bucket
@@ -6,6 +10,9 @@ module "FirstLambda" {
   handler              = "hello-python.lambda_handler"
   environment          = var.environment
   layer_arn            = aws_lambda_layer_version.existing_lambda_layer.arn
+  vpc_id               = module.network.vpc_id
+  private_subnets_id   = module.network.private_subnets_id
+  depends_on = [module.network]
 }
 
 module "SecondLambda" {
@@ -17,4 +24,7 @@ module "SecondLambda" {
   environment          = var.environment
   #layer_arn            = aws_lambda_layer_version.existing_lambda_layer.arn
   layer_arn            = ""
+  vpc_id               = module.network.vpc_id
+  private_subnets_id   = module.network.private_subnets_id
+  depends_on = [module.network]
 }
