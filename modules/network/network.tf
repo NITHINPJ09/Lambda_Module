1b7f31af999f65de998029325c9140e86d2089c1
Nithin Prince John <nithinpj09@gmail.com>
2023-12-20 11:37:26 +0530

    Update network.tf

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -1,1 +1,26 @@
+data "aws_vpc" "existing_vpc" {
+  filter {
+    name   = "tag:Environment"
+    values = ["dev"]
+  }
 
+  filter {
+    name   = "tag:Name"
+    values = ["*vpc*"]
+  }
+}
+
+
+data "aws_subnets" "private_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing_vpc.id]
+  }
+
+  // Filter to select private subnets based on your criteria
+  filter {
+    name   = "tag:Name"
+    values = ["*private-subnet*"]
+  }
+  
+}
