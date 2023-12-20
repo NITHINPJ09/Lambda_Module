78e65cedc014dfe2244520d8fa6000192a89fa9a
Nithin Prince John <nithinpj09@gmail.com>
2023-12-20 11:46:50 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -23,7 +23,7 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   layers = [var.layer_arn]
   #layers = []
   vpc_config {
-    subnet_ids         = data.aws_subnets.private_subnets.ids
+    subnet_ids         = var.private_subnets_id
     security_group_ids = [aws_security_group.lambda_sg.id] # Add security group IDs if necessary
   }
   depends_on = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role, data.aws_s3_bucket.existing_lambda_bucket, aws_cloudwatch_log_group.example]
